f4c2c930dda157a8dcc2b77973303274086fa6df
Aryamannn <mishraaryaman2003@gmail.com>
2026-04-03 20:58:04 -0700

    Add diagnostic error catching to serverless function

--- api/index.js
+++ api/index.js
@@ -1,2 +1,10 @@
-const handler = require("./handler.cjs");
-module.exports = handler.default ?? handler;
+let app;
+try {
+  const handler = require("./handler.cjs");
+  app = handler.default ?? handler;
+} catch (e) {
+  app = (req, res) => {
+    res.status(500).json({ error: "Handler load failed", message: e.message, stack: e.stack });
+  };
+}
+module.exports = app;
